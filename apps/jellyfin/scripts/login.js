00bd7cb53693adc4871ba5a5b993fe5c888c26b3
tbvns <tbvns601@gmail.com>
2026-04-19 03:04:34 +0200

    Better credential management

--- apps/jellyfin/scripts/login.js
+++ apps/jellyfin/scripts/login.js
@@ -3,7 +3,14 @@ const body = JSON.parse(values.BODY)
 
 result = values.BODY;
 
-if (auth.getUsernameFromToken(token) === body.Username) {
-    body.Pw = auth.getKeyForToken(token);
-    result = JSON.stringify(body);
+if (token) {
+    if (auth.getUsernameFromToken(token) === body.Username) {
+        body.Pw = auth.getKeyForToken(token);
+        result = JSON.stringify(body);
+    }
+} else {
+    if (auth.validateCredentials(body.Username, body.Pw)) {
+        body.Pw = auth.getKeyForUsername(body.Username);
+        result = JSON.stringify(body);
+    }
 }
